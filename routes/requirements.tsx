4450eab4fc6874ad32b2e4d955359ade5eb18e7c
tokimi <ben@tokimi.space>
2026-01-14 03:19:07 +0800

    add ui

---
+++ routes/requirements.tsx
@@ -0,0 +1,12 @@
+import { type Handlers } from "fresh";
+
+export const handler: Handlers = {
+  async GET(_req) {
+    const html = await Deno.readTextFile("./requirements.html");
+    return new Response(html, {
+      headers: {
+        "content-type": "text/html; charset=utf-8",
+      },
+    });
+  },
+};
